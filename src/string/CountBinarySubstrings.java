acae5a14aa0cd77e60d2368beb1dfbcc1cb9c810
HinWill <934278385@qq.com>
2017-12-23 10:23:08 +0800

    :memo:

---
+++ src/string/CountBinarySubstrings.java
@@ -0,0 +1,43 @@
+package string;
+
+
+/*
+Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+Substrings that occur multiple times are counted the number of times they occur.
+
+给定一个由0和1组成的非空字符串，计算出由相同0和1且0和1分别连续的子串的个数。子串可以重复。
+
+思路：使用2个变量来存储当前数字前的数字连续次数pre以及当前数字的连续次数cur。
+如果当前数字与前一个数字连续，则计算出当前数字连续的次数cur，否则统计当前数字之前的数字连续次数pre并令当前数字连续次数cur为1。
+接着通过判断统计子数组的个数，如果这时该数字之前的数字连续次数pre大于等于当前数字连续次数cur，则令子数组个数res加1。
+
+如果不理解，按照该代码自行调试一遍，列出每次res加1所对应的子数组方便理解。
+
+例如 “00110”，存在连续子数组“01”，“0011”，“10”。
+
+
+Your runtime beats 25.49 % of java submissions.
+
+ */
+public class CountBinarySubstrings {
+
+    public int countBinarySubstrings(String s) {
+        int prevRunLength = 0, curRunLength = 1, res = 0;
+        for (int i=1;i<s.length();i++) {
+            if (s.charAt(i) == s.charAt(i-1)) curRunLength++;
+            else {
+                prevRunLength = curRunLength;
+                curRunLength = 1;
+            }
+            if (prevRunLength >= curRunLength) res++;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int num = new CountBinarySubstrings().countBinarySubstrings("0101");
+    }
+
+}
---
+++ src/string/ReverseStringII.java
@@ -0,0 +1,50 @@
+package string;
+
+
+/*
+Given a string and an integer k, you need to reverse the first k characters for every 2k
+characters counting from the start of the string. If there are less than k characters left,
+reverse all of them. If there are less than 2k but greater than or equal to k characters,
+ then reverse the first k characters and left the other as original.
+
+题目不难，需要看清题意。输入一个字符串s和一个整数k，使得前k个字符串翻转，后k个字符串保持不变，如此不断处理整个字符串，直到达到字符串尾部。
+
+
+实现方法：
+1.先写一个函数void reverse(string &s,int start,int end)；实现字符串s从start到end的翻转。
+2.然后需要一个变量存下一次的终止位置，终止位置取当前位置+k-1 和 字符串尾。
+3.另外还需要判断的是，在翻转的时候，当end大于字符串的长度时，则应该翻转start到s.length()-1之间的字符串。
+
+我的代码多考虑了剩余字符串长度的判断
+ */
+
+public class ReverseStringII {
+    public String reverseStr(String s, int k) {
+
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+
+        int i = 0;
+
+        while (i < len){
+            int  j = Math.min(i+k-1,len-1);
+
+            swap(arr, i, j);
+
+            i += 2*k;
+        }
+       return String.valueOf(arr);
+    }
+    public void swap(char[] str , int m, int n){
+        while (m < n){
+            char t= str[m];
+            str[m++] = str[n];
+            str[n--] = t;
+        }
+    }
+    public static void main(String[] args) {
+        String a = "abc";
+        a = new ReverseStringII().reverseStr(a ,3);
+        System.out.println("a = " + a);
+    }
+}
