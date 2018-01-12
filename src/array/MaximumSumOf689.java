866245f128a6cc6678f02af39afc467cd1bcc560
HinWill <934278385@qq.com>
2018-01-12 20:47:57 +0800

    :memo:

---
+++ src/array/MaximumSumOf689.java
@@ -0,0 +1,18 @@
+package array;
+
+public class MaximumSumOf689 {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+
+
+        // TODO: 2018/1/12
+
+        return nums;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+
+    }
+}
