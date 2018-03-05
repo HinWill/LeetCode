f9c7cae31d79f7f3adfe15d8882db3726bf34c57
HinWill <934278385@qq.com>
2018-03-05 19:40:13 +0800

    :memo:

--- src/array/ProduceOfArrayExceptSelf238.java
+++ src/array/ProduceOfArrayExceptSelf238.java
@@ -1,5 +1,29 @@
 package array;
 
+/*
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+
+ 题目描述：给出一个数组，返回一个除当前元素之外元素之积的数组
+ 要求：不能使用除法，限制时间复杂度为0(n),并且使用恒定的空间复杂度
+ 思路：看到这个题有点眼熟，知道是使用当前元素的前后个元素之积相乘，想到这里思路也就出来了。举个例子
+              数组：{ n1,n2,n3,n4}
+ 那么所要求的数组为：{n2*n3*n4, n1*n3*n4, n1*n2*n4, n1*n2*n3}
+        可以看做是：
+        {       1，     n1,   n1*n2,   n1*n2*n3}  -> a1
+       *{n2*n3*n4,   n3*n4,      n4,   1}         -> a2
+
+       但是这里只需要创建一个数组就可以了，使用一个变量存储数组a2的值就可以了
+
+ */
+
+
 public class ProduceOfArrayExceptSelf238 {
 
     public int[] productExceptSelf(int[] nums) {
