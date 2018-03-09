28e7da945aa2809c99a5d7018073fa62e5fb8f71
HinWill <934278385@qq.com>
2018-03-09 21:47:41 +0800

    :memo:

--- src/string/ValidParentheses20.java
+++ src/string/ValidParentheses20.java
@@ -1,26 +1,66 @@
 package string;
 
+
+/*
+
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+
+题目描述：给出一组括号，括号是不是一一对应的，比如 ( )  或者 { } [ ] ( )算合法的括号，（] 或者 ( [ ) ] 都不合法。
+
+思路：这道题用loop 就非常的麻烦，但是用栈就是非常的简单，其实这道题就是考察的对栈的使用
+具体的思路如下：将前括号压入栈中，如果栈顶遇到与之相匹配后括号就弹出，最后检查栈是否为空，如果为空那么就说明全部为合法的括号
+
+导论区的写法更加简练
+
+Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+
+
+ */
+
+import java.util.Stack;
+
 public class ValidParentheses20 {
 
     public boolean isValid(String s) {
-        int n = 0;
-        for (int i = 0 ; i < s.length()-1;i=i+2){
-            char c = s.charAt(i);
-            char u = s.charAt(i+1);
-            n = 0;
-            if ((c == '(' && u == ')') || (c == '{'&& u == '}' )|| (c == '[' && u == ']')){
-                n = 1;
-            }
-            if (n == 0){
-                break;
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+        stack.push(arr[0]);
+
+        for (int i = 1; i < s.length(); i++){
+            if (!stack.isEmpty() && isMatch(stack.peek(),arr[i])){
+                stack.pop();
+            }else {
+                stack.push(arr[i]);
             }
         }
-        return n == 1;
+
+        return stack.isEmpty();
+
+
+    }
+
+    public boolean isMatch(char a, char b){
+        return (a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}');
     }
 
     public static void main(String[] args) {
 
-        boolean b = new ValidParentheses20().isValid("(");
+        boolean b = new ValidParentheses20().isValid("()");
 
         System.out.println("b = " + b);
     }
