d96564c8552b3ba87f92f78d1902a47314e6b96c
HinWill <934278385@qq.com>
2018-03-15 21:36:34 +0800

    :memo:

---
+++ src/DynamicProgramming/RangeSumQueryImmutable303.java
@@ -0,0 +1,22 @@
+package DynamicProgramming;
+
+/*
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+
+题目描述：给出一组数，求指定区间的和，给出的条件是数组内容不会变化，这里有很多次对sumRange函数的调用
+
+ */
+
+
+public class RangeSumQueryImmutable303 {
+}
